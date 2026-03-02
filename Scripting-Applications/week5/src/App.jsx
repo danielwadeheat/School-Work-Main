f4064aa99e97197a7852d00f2b1c8a7503c1a2a4
Daniel Heatherly <dh1122808@student.francistuttle.edu>
2026-03-01 18:41:46 -0600

    Week5 finished

--- Scripting-Applications/week5/src/App.jsx
+++ Scripting-Applications/week5/src/App.jsx
@@ -5,7 +5,7 @@ import ProductList from "./ex3/ProductList";
 export default function App() {
   return (
     <main className="page">
-      <h1>Week 5 - React Exercises</h1>
+      <h1>Week 5 - Vite React Exercises</h1>
       <TaskBoard />
       <GreetingForm />
       <ProductList />
